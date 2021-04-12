830f18372d1376ab1cca33fac7399886309dd113
Albin158 <70146013+Albin158@users.noreply.github.com>
2021-04-12 15:21:13 +0200

    listor for

---
+++ listor for.py
@@ -0,0 +1,8 @@
+la = [7, 14, 21, 28]    #typen list
+ta = (5, 10, 15)        #typen tuple
+tb = 3, 6, 9, 12  # typen tuple
+
+l0 = []  #tom lista
+t0 = ()  #tom tupel
+l1 = ['ensam']  #en singleton
+t1 = ('singel') #en singleton
---
+++ string.py
@@ -0,0 +1,2 @@
+Albin = "dumbo"
+print(Albin)
